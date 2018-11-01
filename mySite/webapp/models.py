f5812f1e72359b67dbd806318b195523c079e2b7
aklomkaew <ak0061@uah.edu>
2018-11-01 09:26:14 -0500

    Created json payload

--- mySite/webapp/models.py
+++ mySite/webapp/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+# class Post(models.Model):
+#     post = models.CharField(max_length=500)
+#     user = models.ForeignKey(User)
 
-# Create your models here.
+
+# import webapp.models
+#
+# ticket = Ticket()
+# ticket.firstname = "john"
+# ticket.date = ""
+# ticket.Save()
+#
+# ticket = Ticket.filter(name="john")
