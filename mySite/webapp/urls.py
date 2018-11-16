48bb148dadd7211e81d5848882a07c2f031d291a
ajt0022 <39925222+ajt0022@users.noreply.github.com>
2018-11-16 14:22:06 -0600

    Dynamic performance page (#17)
    * Dynamically create performance cards
    
    * Add func in views
    
    * Almost done
    
    * Performances page

--- mySite/webapp/urls.py
+++ mySite/webapp/urls.py
@@ -4,12 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('performance/', views.performance, name='performance'),
+    path('getPerformances/', views.getPerformances, name='getPerformances'),
+    path('getPerformances/<str:theater>/<str:month>/<str:day>/<str:year>', views.getPerformances, name='getPerformancesARGS'),
     path('contact/', views.contact, name='contact'),
     path('buySeasonTickets/', views.buySeasonTicket, name='buySeasonTickets'),
     path('admin/', views.admin, name='admin'),
     path('payment/', views.payment, name='payment'),
     path('seatSelection/', views.seatSelection, name='seatSelection'),
+    path('seatSelection/<str:theater>/<str:day>/<str:time>/', views.seatSelection, name='seatSelectionARGS'),
     path('seasonSeatSelection/<str:season>/<str:theater>/<str:day>/<str:time>/', views.seasonSeatSelection, name='seasonSeatSelection'),
-    path('seatSelection/concertHall/', views.concertHall, name='concertHall'),
+    path('concertHall/', views.concertHall, name='concertHall'),
     path('confirmationPage/<str:seat_numbers>/', views.confirmationPage, name='confirmationPage')
 ]
