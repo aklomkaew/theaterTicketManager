76c7963c379c40caecd4b71cf30bf32fc6194ef8
atowers23 <atowers23@gmail.com>
2018-11-28 22:58:13 -0600

    Add docs

--- mySite/update.sh
+++ mySite/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull
 docker-compose run web python3 manage.py makemigrations
 docker-compose run web python3 manage.py migrate
 docker-compose run web python3 manage.py loaddata data_set.json
